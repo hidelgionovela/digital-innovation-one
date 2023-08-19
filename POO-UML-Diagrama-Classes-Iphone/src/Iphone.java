f8102f885d54ec63bab6ff7165dd1544a0e5ad12
hidelgionovela <89780156+hidelgionovela@users.noreply.github.com>
2023-08-19 02:39:27 +0200

    Chore: implementing all interfaces

---
+++ POO-UML-Diagrama-Classes-Iphone/src/Iphone.java
@@ -0,0 +1,60 @@
+
+public class Iphone implements NavegadorInternet, AparelhoTelefonico, ReprodutorMusical {
+
+  @Override
+  public void tocar() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'tocar'");
+  }
+
+  @Override
+  public void pausar() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'pausar'");
+  }
+
+  @Override
+  public void selecionarMusica() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'selecionarMusica'");
+  }
+
+  @Override
+  public void ligar() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'ligar'");
+  }
+
+  @Override
+  public void atender() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'atender'");
+  }
+
+  @Override
+  public void iniciarCorreio() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'iniciarCorreio'");
+  }
+
+  @Override
+  public void exibirPagina() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'exibirPagina'");
+  }
+
+  @Override
+  public void adcionarAba() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'adcionarAba'");
+  }
+
+  @Override
+  public void actualizarPagina() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'actualizarPagina'");
+  }
+
+
+
+}
